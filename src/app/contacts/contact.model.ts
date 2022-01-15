ceb72afc4ac24b4d99b0aa8da2731130e748f62e
Tyler Tucker <webcooltz@duck.com>
2022-01-15 13:34:16 -0700

    assignment week 2

---
+++ src/app/contacts/contact.model.ts
@@ -0,0 +1,19 @@
+
+
+export class Contact {
+  public id: number;
+  public name: string;
+  public email: string;
+  public phone: string;
+  public imageUrl: string;
+  public group: string;
+
+  constructor(id: number, name: string, email: string, phone: string, imageUrl: string, group: string) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.imageUrl = imageUrl;
+    this.group = group;
+  }
+}
---
+++ src/app/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: 'header.component.html',
+  styleUrls: ['app.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
